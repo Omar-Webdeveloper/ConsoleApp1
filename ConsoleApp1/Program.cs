0b83f9a8677ca5e3db7a6ffff7a91884e85e44c3
Omar-Webdeveloper <omarabojolban9@gmail.com>
2025-01-16 16:42:05 +0300

    All done for this task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,10 +15,15 @@ namespace ConsoleApp1
         {
             //1 - Create console application that read string from user and print the same string
             Console.WriteLine("Task 1");
-
+            Console.WriteLine(" read string from user  ");
             string read_line = Console.ReadLine();
-            Console.WriteLine(read_line);
+            Console.WriteLine( read_line);
             Console.WriteLine("");
+            Console.WriteLine("Task 1 Completed Successflly");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("*****************************************************");
 
             //2 - Define variables with kind of
             //a.Double
@@ -35,15 +40,31 @@ namespace ConsoleApp1
             char letter ='A';
             bool isTrue = false;
             int number_2 = 2;
-            const int number_3 = 3;
+            int number_3 = 3;
 
-            Console.WriteLine(number_1);
-            Console.WriteLine(text);
-            Console.WriteLine(letter);
-            Console.WriteLine(isTrue);
-            Console.WriteLine(number_2);
-            Console.WriteLine(number_3);
+            Console.WriteLine(" double number_1 = " + number_1);
+            Console.WriteLine("");
+            Console.WriteLine(" string text = " + text);
+            Console.WriteLine("");
 
+            Console.WriteLine(" char letter = " + letter);
+            Console.WriteLine("");
+
+            Console.WriteLine(" bool isTrue = = " + isTrue);
+            Console.WriteLine("");
+
+            Console.WriteLine(" int number_2 = " + number_2);
+            Console.WriteLine("");
+
+            Console.WriteLine(" const int number_3 = " + number_3);
+            Console.WriteLine("");
+
+
+            Console.WriteLine("Task 2 Completed Successflly");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("*****************************************************");
 
 
 
@@ -54,7 +75,14 @@ namespace ConsoleApp1
             foreach (string car in cars)
             {
                 Console.WriteLine(car);
+                Console.WriteLine("");
             }
+
+            Console.WriteLine("Task 3 Completed Successflly");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("*****************************************************");
 
 
 
@@ -64,11 +92,24 @@ namespace ConsoleApp1
             //    •	Input your year of birth: 1999
             //    •	Sami Ali 1999
             Console.WriteLine("Task 4");
+
+            Console.WriteLine(" Input your firstname:  ");
             string firstname = Console.ReadLine();
+
+            Console.WriteLine(" Input your lastname:  ");
             string lastname = Console.ReadLine();
+
+            Console.WriteLine(" Input your year of birth:  ");
             string year_of_birth = Console.ReadLine();
 
             Console.WriteLine(firstname + lastname + year_of_birth);
+
+
+            Console.WriteLine("Task 4 Completed Successflly");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("*****************************************************");
 
 
 
@@ -81,8 +122,27 @@ namespace ConsoleApp1
             //    •	.......
             //    •	Expected Output :
             //    •	Elements in array are: 1 1 2 3 4 5 6 7 8 9
+            Console.WriteLine("TASK 5");
+
+            int[] my_elements = new int[10];  
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                my_elements[i] = Convert.ToInt32(Console.ReadLine());  
+            }
+
+            foreach (int element in my_elements)
+            {
+                Console.WriteLine(element);
+            }
 
 
+            Console.WriteLine("Task 5 Completed Successflly");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
 
         }
     }
